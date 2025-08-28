f3da2bc6be42dec61109ffba5ec8d42d68a2a5e7
AlHas786 <alharrison786@gmail.com>
2025-08-28 00:03:03 -0700

    Add files via upload

---
+++ Student.cpp
@@ -0,0 +1,28 @@
+#include "Student.hpp"
+
+
+Student::Student() {
+    cout << "Student object created!" << endl;
+}
+
+
+Student::~Student() {
+    cout << "Student object destroyed!" << endl;
+}
+
+
+void Student::setName(string n) {
+    name = n;
+}
+
+void Student::setAge(int a) {
+    age = a;
+}
+
+string Student::getName() const {
+    return name;
+}
+
+int Student::getAge() const {
+    return age;
+}
